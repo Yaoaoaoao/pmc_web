93f36f834aeaae0320cfa64f1b8edcfe06623cc5
Jia Ren <renj@biotm2.cis.udel.edu>
2017-06-24 00:51:26 -0400

    modify json retrieve temporary script

--- pymongo_query.py
+++ pymongo_query.py
@@ -15,6 +15,9 @@ def get_data(db, collection, key, value):
     cursor = client[db][collection].find_one({key: value}, {'_id': 0})
     # temp
     for k in cursor['entity'].keys():
-        cursor['entity'][k]['entityText'] = cursor['entity'][k]['text']
-        del cursor['entity'][k]['text']
+        if 'text' in cursor['entity'][k]:
+            cursor['entity'][k]['entityText'] = cursor['entity'][k]['text']
+            del cursor['entity'][k]['text']
+        else:
+            break
     return cursor
