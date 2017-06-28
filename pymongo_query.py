aa708fe68e90c683fccb11e1b2f728729d487459
Jia Ren <renj@biotm2.cis.udel.edu>
2017-06-27 20:11:11 -0400

    remove temp clean json function

--- pymongo_query.py
+++ pymongo_query.py
@@ -12,12 +12,5 @@ def get_section_list(db, collection, pmcid):
 
 
 def get_data(db, collection, key, value):
-    cursor = client[db][collection].find_one({key: value}, {'_id': 0})
-    # temp
-    for k in cursor['entity'].keys():
-        if 'text' in cursor['entity'][k]:
-            cursor['entity'][k]['entityText'] = cursor['entity'][k]['text']
-            del cursor['entity'][k]['text']
-        else:
-            break
-    return cursor
+    return client[db][collection].find_one({key: value}, {'_id': 0})
+
